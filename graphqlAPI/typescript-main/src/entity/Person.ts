1c539dd86d880c6c90fe1c400f5592f06d683f95
Jose Ignacio Naranjo <52728126+123-code@users.noreply.github.com>
2023-09-30 22:54:33 +0000

    crud ops

--- graphqlAPI/typescript-main/src/entity/Person.ts
+++ graphqlAPI/typescript-main/src/entity/Person.ts
@@ -1,18 +1,23 @@
 import { Entity,PrimaryGeneratedColumn,Column, BaseEntity } from "typeorm";
+import { Field, Int, ObjectType } from "type-graphql";
 
+@ObjectType()
 @Entity()
 export class Person extends BaseEntity{
+
+@Field(()=> Int)
 @PrimaryGeneratedColumn()
 id: number;
 
+@Field()
 @Column()
 name:string;
 
-
+@Field()
 @Column()
 request:string;
 
-
+@Field(()=> Int)
 @Column('int',{default:20000})
 amount:number;
 
--- graphqlAPI/typescript-main/src/resolvers/PersonResolver.ts
+++ graphqlAPI/typescript-main/src/resolvers/PersonResolver.ts
@@ -1,5 +1,19 @@
 import { Person } from "../entity/Person";
-import { Resolver, Mutation, Arg, Int } from "type-graphql";
+import { Resolver, Mutation, Arg, Int,Query } from "type-graphql";
+
+
+/*
+class PersonInput{
+@Field ()
+name:string;
+
+@Field(()=>Int)
+amount:number;
+
+@Field()
+request:string;
+}
+*/
 
 @Resolver()
 
@@ -14,6 +28,27 @@ async createPerson(
 }
 
 
+@Mutation(()=>Boolean)
+
+async updatePerson(
+    
+    @Arg("id",()=>Int) id: number,@Arg("name")name:string,@Arg('amount',()=>Int)amount:number ,@Arg("request")request:string){
+   await  Person.update({id},{name,amount,request})
+    return true;
+}
+
+@Mutation(()=>Boolean)
+async DeletePerson(
+    @Arg("id",()=>Int) id:number){
+        await Person.delete({id});
+        return true
+    }
+
+   
+@Query(()=>[Person])
+persons(){
+    return Person.find();
+}
 
 
 
